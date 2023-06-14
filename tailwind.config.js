3f500ff53cc20428fca112c8ebd5d0a636c2dc1a
unknown <kakarotoroth@gmail.com>
2023-06-13 20:24:26 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  // darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        'primary2': '#104C77',
+        'primary': '#0B3452',
+        'black2': '#363434',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
+    },
+    
+  variants: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
